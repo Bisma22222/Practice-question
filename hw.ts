4d14af122fae33451dea102ef2c7a1ae2c699393
Bisma22222 <165092471+Bisma22222@users.noreply.github.com>
2024-03-29 02:53:05 +0500

    Add files via upload

---
+++ hw.ts
@@ -0,0 +1,19 @@
+// Ticket checker Assignment
+
+const age = 20;  
+if(age>=6 && age<=12){
+    console.log("You are eligible for a Child ticket")
+}
+else if(age>12 && age<=20){
+    console.log("You are eligible for Teenager ticket")
+}
+else if(age>20 && age<=40){
+    console.log("You are eligible for Adult ticket")
+}
+else if(age>40 && age<=60){
+    console.log("You are eligible for old-people ticket")
+}
+else{
+    console.log("You are eligible for senior citizen ticket")
+
+}
